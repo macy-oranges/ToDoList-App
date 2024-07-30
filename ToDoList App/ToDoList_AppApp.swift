66a3d8c2e9b8a3e009eef30dd1f3296379969dca
Macy <wonganqi888@gmail.com>
2024-07-30 09:49:36 -0700

    Initial Commit

---
+++ ToDoList App/ToDoList_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_AppApp.swift
+//  ToDoList App
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
